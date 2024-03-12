d2b7b82428d20f3136db1ab883fdd40dd59c6e2b
y2kchen <y2kchen@gmail.com>
2024-03-11 17:59:08 -0700

    Initial commit

---
+++ Manager.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Manager
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCurrent = New System.Windows.Forms.Button()
+        Me.tbPath = New System.Windows.Forms.TextBox()
+        Me.btnSetPath = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BtnLaunch = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RBTimeDescend = New System.Windows.Forms.RadioButton()
+        Me.RBTimeAscend = New System.Windows.Forms.RadioButton()
+        Me.BtnGenScr = New System.Windows.Forms.Button()
+        Me.TbScrCor = New System.Windows.Forms.TextBox()
+        Me.tbDumpFNameSuffix = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCurrent
+        '
+        Me.btnCurrent.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnCurrent.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCurrent.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnCurrent.Location = New System.Drawing.Point(383, 418)
+        Me.btnCurrent.Name = "btnCurrent"
+        Me.btnCurrent.Size = New System.Drawing.Size(90, 20)
+        Me.btnCurrent.TabIndex = 77
+        Me.btnCurrent.Text = "Current Path"
+        Me.btnCurrent.UseVisualStyleBackColor = False
+        Me.btnCurrent.Visible = False
+        '
+        'tbPath
+        '
+        Me.tbPath.Location = New System.Drawing.Point(29, 277)
+        Me.tbPath.Name = "tbPath"
+        Me.tbPath.Size = New System.Drawing.Size(433, 20)
+        Me.tbPath.TabIndex = 76
+        Me.tbPath.Text = "C:\Evolver\fe"
+        Me.tbPath.WordWrap = False
+        '
+        'btnSetPath
+        '
+        Me.btnSetPath.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnSetPath.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSetPath.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnSetPath.Location = New System.Drawing.Point(29, 251)
+        Me.btnSetPath.Name = "btnSetPath"
+        Me.btnSetPath.Size = New System.Drawing.Size(90, 20)
+        Me.btnSetPath.TabIndex = 75
+        Me.btnSetPath.Text = "Browse"
+        Me.btnSetPath.UseVisualStyleBackColor = False
+        '
+        'BtnLaunch
+        '
+        Me.BtnLaunch.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.BtnLaunch.Location = New System.Drawing.Point(29, 12)
+        Me.BtnLaunch.Name = "BtnLaunch"
+        Me.BtnLaunch.Size = New System.Drawing.Size(162, 23)
+        Me.BtnLaunch.TabIndex = 78
+        Me.BtnLaunch.Text = "Launch SE-FIT"
+        Me.BtnLaunch.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(197, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 79
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(29, 41)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(433, 197)
+        Me.RichTextBox1.TabIndex = 80
+        Me.RichTextBox1.Text = ""
+        '
+        'RBTimeDescend
+        '
+        Me.RBTimeDescend.AutoSize = True
+        Me.RBTimeDescend.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBTimeDescend.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.RBTimeDescend.Location = New System.Drawing.Point(291, 254)
+        Me.RBTimeDescend.Name = "RBTimeDescend"
+        Me.RBTimeDescend.Size = New System.Drawing.Size(123, 17)
+        Me.RBTimeDescend.TabIndex = 86
+        Me.RBTimeDescend.Text = "Time Descending"
+        Me.RBTimeDescend.UseVisualStyleBackColor = True
+        '
+        'RBTimeAscend
+        '
+        Me.RBTimeAscend.AutoSize = True
+        Me.RBTimeAscend.Checked = True
+        Me.RBTimeAscend.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RBTimeAscend.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.RBTimeAscend.Location = New System.Drawing.Point(170, 254)
+        Me.RBTimeAscend.Name = "RBTimeAscend"
+        Me.RBTimeAscend.Size = New System.Drawing.Size(115, 17)
+        Me.RBTimeAscend.TabIndex = 85
+        Me.RBTimeAscend.TabStop = True
+        Me.RBTimeAscend.Text = "Time Ascending"
+        Me.RBTimeAscend.UseVisualStyleBackColor = True
+        '
+        'BtnGenScr
+        '
+        Me.BtnGenScr.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.BtnGenScr.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnGenScr.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnGenScr.Location = New System.Drawing.Point(29, 422)
+        Me.BtnGenScr.Name = "BtnGenScr"
+        Me.BtnGenScr.Size = New System.Drawing.Size(90, 20)
+        Me.BtnGenScr.TabIndex = 87
+        Me.BtnGenScr.Text = "Generate Script"
+        Me.BtnGenScr.UseVisualStyleBackColor = False
+        '
+        'TbScrCor
+        '
+        Me.TbScrCor.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.TbScrCor.Location = New System.Drawing.Point(29, 326)
+        Me.TbScrCor.Multiline = True
+        Me.TbScrCor.Name = "TbScrCor"
+        Me.TbScrCor.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TbScrCor.Size = New System.Drawing.Size(433, 90)
+        Me.TbScrCor.TabIndex = 88
+        Me.TbScrCor.Text = "{g 5; V; u}100" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.TbScrCor.WordWrap = False
+        '
+        'tbDumpFNameSuffix
+        '
+        Me.tbDumpFNameSuffix.Location = New System.Drawing.Point(165, 301)
+        Me.tbDumpFNameSuffix.Name = "tbDumpFNameSuffix"
+        Me.tbDumpFNameSuffix.Size = New System.Drawing.Size(88, 20)
+        Me.tbDumpFNameSuffix.TabIndex = 90
+        Me.tbDumpFNameSuffix.Text = "_n"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label1.Location = New System.Drawing.Point(35, 304)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 13)
+        Me.Label1.TabIndex = 91
+        Me.Label1.Text = "Dump filename suffix"
+        '
+        'Manager
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 451)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbDumpFNameSuffix)
+        Me.Controls.Add(Me.TbScrCor)
+        Me.Controls.Add(Me.BtnGenScr)
+        Me.Controls.Add(Me.RBTimeDescend)
+        Me.Controls.Add(Me.RBTimeAscend)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnLaunch)
+        Me.Controls.Add(Me.btnCurrent)
+        Me.Controls.Add(Me.tbPath)
+        Me.Controls.Add(Me.btnSetPath)
+        Me.MaximizeBox = False
+        Me.Name = "Manager"
+        Me.Text = "SE-FIT Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCurrent As Button
+    Friend WithEvents tbPath As TextBox
+    Friend WithEvents btnSetPath As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BtnLaunch As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RBTimeDescend As RadioButton
+    Friend WithEvents RBTimeAscend As RadioButton
+    Friend WithEvents BtnGenScr As Button
+    Friend WithEvents TbScrCor As TextBox
+    Friend WithEvents tbDumpFNameSuffix As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Manager.vb
@@ -0,0 +1,265 @@
+﻿Imports System.Threading.Tasks
+Imports System
+Imports System.Diagnostics
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Manager
+    Dim cores As Integer = 0
+    Dim bwdpath As String = ""
+    Dim fwdpath As String = ""
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+
+        bwdpath = Path.GetDirectoryName(Application.ExecutablePath) + "\"
+        fwdpath = bwdpath.Replace("\", "/")
+
+        'Static Dim n As Integer = 0
+        Static Dim maxProcessorAffinity As Integer = 0
+        Dim i, id, core64(64) As Integer
+        BtnLaunch.Enabled = True
+        Try
+            If maxProcessorAffinity = 0 Then
+                'n += 1
+                ' Get all processes running on the local computer. 
+                Dim localAll As Process() = Process.GetProcesses()
+
+                For Each p As Process In localAll
+                    Try
+                        If maxProcessorAffinity < p.ProcessorAffinity.ToInt64 Then
+                            maxProcessorAffinity = p.ProcessorAffinity.ToInt64
+                        End If
+                    Catch ex As Exception
+
+                    End Try
+                Next
+
+                Dim temp = maxProcessorAffinity
+                While temp > 0
+                    temp \= 2
+                    cores += 1
+                End While
+                Me.Text = "SE-FIT Launcher detects " + cores.ToString + " cores."
+                Label2.Text = "(0 - " + (cores - 1).ToString + ")"
+                'NumericUpDown1.Maximum = cores - 1
+            End If
+            RichTextBox1.Clear()
+            For id = 0 To cores - 1
+                core64(id) = 0
+            Next
+            Dim localByName As Process() = Process.GetProcessesByName("SE-FIT")
+            If localByName.Length > 0 Then
+                For Each p As Process In localByName
+                    RichTextBox1.AppendText("SE-FIT process " + p.Id.ToString + ", affinity: " + p.ProcessorAffinity.ToString() + ", core: ")
+                    i = 1
+                    id = 0
+                    While id < cores
+                        If p.ProcessorAffinity.ToInt64 And i Then
+                            RichTextBox1.AppendText(id.ToString + "  ")
+                            core64(id) += 1
+                        End If
+                        i *= 2
+                        id += 1
+                    End While
+                    RichTextBox1.AppendText(vbNewLine)
+                Next
+            Else
+
+            End If
+            id = 0
+            For i = 1 To cores - 1 Step 1
+                If core64(id) > core64(i) Then
+                    id = i
+                End If
+            Next
+
+
+            'NumericUpDown1.Value = id
+            'RichTextBox1.AppendText("Target core # " + id.ToString + vbNewLine)
+
+        Catch ex As Exception
+            BtnLaunch.Enabled = False
+            MessageBox.Show("Could not access process list.", "Error!")
+        End Try
+    End Sub
+
+    Dim fileList As System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+    Dim orderedFilesByCreationTime As New System.Collections.Generic.List(Of String)
+    Private Sub btnSetPath_Click(sender As Object, e As EventArgs) Handles btnSetPath.Click
+        FolderBrowserDialog1.SelectedPath = tbPath.Text
+        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog()
+
+        If (result = Windows.Forms.DialogResult.OK) Then
+            tbPath.Text = FolderBrowserDialog1.SelectedPath
+        End If
+
+        fileList = My.Computer.FileSystem.GetFiles(
+                    tbPath.Text,
+                    FileIO.SearchOption.SearchTopLevelOnly, "*.dmp")
+        If RBTimeAscend.Checked Then
+            orderedFilesByCreationTime = fileList.ToList().OrderBy(Function(file) New FileInfo(file).CreationTime).ToList()
+        ElseIf RBTimeDescend.Checked Then
+            orderedFilesByCreationTime = fileList.ToList().OrderByDescending(Function(file) New FileInfo(file).CreationTime).ToList()
+        End If
+
+    End Sub
+
+    'Private Sub BtnLaunch_Click(sender As Object, e As EventArgs) Handles BtnLaunch.Click
+    '    For Each foundFile As String In orderedFilesByCreationTime 'fileList
+
+    '    Next
+    '    For i = 1 To cores - 1 Step 2
+    '        RunSEFITSession(i)
+    '    Next
+    '    Form1_Activated(sender, e)
+
+
+    'End Sub
+
+    Async Sub BtnLaunch_Click(sender As Object, e As EventArgs) Handles BtnLaunch.Click
+        '        For i = 1 To cores - 1 Step 2
+
+        For i = 1 To 5 Step 2
+            RunSEFITSession(i)
+            Await Task.Delay(5000)
+        Next
+        Form1_Activated(sender, e)
+
+
+    End Sub
+
+
+    ' In an asynchronous method
+
+
+    'Async Function RunSEFITSessionAsync(sessionParams As Integer) As Task(Of String)
+
+
+    '    Return Task.Run(Function()
+    '                        ' Code to start and run the SE-FIT session using sessionParams
+    '                        Dim result As SEFITResult = RunSEFITSession(sessionParams) ' Placeholder for actual SE-FIT execution
+    '                        Return result
+    '                    End Function)
+    'End Function
+    'where do I put the following code?
+
+    '    Dim sessions As New List(Of Task(Of SEFITResult))
+    '    For Each param In listOfParameters ' listOfParameters contains the parameters for each session
+    '    sessions.Add(RunSEFITSessionAsync(param))
+    '    Next
+
+    '' Wait for all sessions to complete
+    '    Task.WaitAll(sessions.ToArray())
+
+    '' Or process results as they complete
+    '    For Each session In sessions
+    '    Dim result = Await session
+    '    ' Process result
+    '    Next
+
+    Private Sub RunSEFITSession(x As Integer)
+        Dim a As String = ""
+        'Dim z, x As Integer
+        Dim z As Integer
+        Dim executable As String = "SE-FIT.exe"
+        z = 0
+        'x = NumericUpDown1.Value
+        While x > 3
+            x -= 4
+            z += 1
+        End While
+        Select Case x
+            Case 0
+                a = "1"
+            Case 1
+                a = "2"
+            Case 2
+                a = "4"
+            Case 3
+                a = "8"
+        End Select
+        While z > 0
+            z -= 1
+            a += "0"
+        End While
+        'Me.Text = a
+        'System.Diagnostics.Process.Start()
+        '"C:\Windows\System32\cmd.exe /C START /high /affinity 1 SE-FIT.exe"
+        If Not File.Exists(executable) Then
+            MessageBox.Show("Could not find " + executable + " in the current directory.", "Error!")
+            Return
+        End If
+        Try
+            Dim gSEProcess As New Process()
+            With gSEProcess.StartInfo
+                .FileName = "cmd.exe"
+                '.FileName = "C:\Windows\System32\cmd.exe"
+                '.Arguments = "/C START /high /affinity " + a + " SE-FIT.exe"
+                .Arguments = "/C START /affinity " + a + " " + executable
+                .UseShellExecute = False
+                .CreateNoWindow = True
+                '.RedirectStandardOutput = True
+                '.RedirectStandardError = True
+                '.RedirectStandardInput = True
+                '.WorkingDirectory = bwdpath
+                '.WindowStyle = ProcessWindowStyle.Hidden
+                'If 1 Then
+                '  gSEProcess.ProcessorAffinity = Marshal.ReadInt64(gProcessorAffinity)
+                'End If
+                '.Arguments = ""
+                '.CreateNoWindow = False
+                '.RedirectStandardOutput = False ' WITHOUT THIS LEVOLVER FREEZES AFTER REPEATED DUMP COMMANDS!
+                '.RedirectStandardError = False
+                '.RedirectStandardInput = False
+                '.WindowStyle = ProcessWindowStyle.Normal
+            End With
+            'Start evolver.exe in the background
+            gSEProcess.Start()
+        Catch ex As Exception
+            MessageBox.Show("Could not find " + executable + " in the current directory.", "Error!")
+        End Try
+
+    End Sub
+
+    Private Sub BtnGenScr_Click(sender As Object, e As EventArgs) Handles BtnGenScr.Click
+        Dim dataFileName As String
+        Dim DMPfile As String
+        Dim prefix As String = Now().ToString("yyyyMMdd_HHmmss") + "_"
+
+        Dim filenameAppenndix As String = "_new"
+        filenameAppenndix = tbDumpFNameSuffix.Text
+
+        If File.Exists(bwdpath + "communication\history\" + "Batch_Proc_Setup.txt") Then
+            File.Delete(bwdpath + "communication\history\" + "Batch_Proc_Setup.txt")
+        End If
+
+        Using sw As StreamWriter = New StreamWriter(bwdpath + "communication\history\" + "Batch_Proc_Setup.txt")
+
+            sw.WriteLine(tbPath.Text + "\" + prefix + "batch_script.ses")
+        End Using
+
+        Using sw As StreamWriter = New StreamWriter(tbPath.Text + "\" + prefix + "batch_script.ses")
+
+            For Each foundFile As String In orderedFilesByCreationTime 'fileList
+                dataFileName = foundFile.Substring(foundFile.LastIndexOf("\") + 1)
+                sw.WriteLine("chdir " + """" + tbPath.Text.Replace("\", "/") + """" + vbNewLine)
+                sw.WriteLine("quietload on " + vbNewLine)
+                sw.WriteLine("load " + """" + dataFileName + """" + vbNewLine)
+                sw.WriteLine("quietload off " + vbNewLine)
+                sw.WriteLine("if is_defined(" + """" + "get_all_update" + """" + ") == 0 then {read " + """" + fwdpath + "lib/get_all_update.ses" + """" + "}" + vbNewLine)
+                sw.WriteLine("get_all_update" + vbNewLine)
+                For i = 0 To TbScrCor.Lines.Length - 1
+                    If TbScrCor.Lines(i).Length > 0 Then
+                        sw.WriteLine(TbScrCor.Lines(i) + vbNewLine)
+                    End If
+                Next
+                DMPfile = dataFileName.Substring(0, dataFileName.LastIndexOf("."c))
+                DMPfile = DMPfile + filenameAppenndix + ".dmp"
+                sw.WriteLine("dump " + """" + DMPfile + """" + vbNewLine)
+            Next
+        End Using
+
+    End Sub
+End Class
